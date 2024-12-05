4f2a8913c6a728f3a18446d55341e4006fdfc5ea
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-05 19:33:09 +0530

    styles

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Sample posts data
+let posts = [
+    {
+        username: "pogo",
+        content: "I love cake",
+    },
+    {
+        username: "dingo",
+        content: "I love ice cream",
+    },
+    {
+        username: "micky",
+        content: "I love apple",
+    },
+];
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Server is working well!");
+});
+
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
